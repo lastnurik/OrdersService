d9bcaf1923c127ace811d71443f65e6349dac176
lastnurik <es.nurik3@gmail.com>
2025-10-28 14:54:42 +0500

    Cancellation token required внутри интерфейса

--- OrdersService.Domain/Repositories/IOrderRepository.cs
+++ OrdersService.Domain/Repositories/IOrderRepository.cs
@@ -4,10 +4,10 @@ namespace OrdersService.Domain.Repositories
 {
     public interface IOrderRepository
     {
-        Task<Order?> GetByIdAsync(Guid id, CancellationToken ct = default);
-        Task<(List<Order> Items, int TotalCount)> GetAllAsync(int pageNumber, int pageSize, CancellationToken ct = default);
-        Task AddAsync(Order order, CancellationToken ct = default);
-        Task UpdateAsync(Order order, CancellationToken ct = default);
-        Task<bool> DeleteAsync(Guid id, CancellationToken ct = default);
+        Task<Order?> GetByIdAsync(Guid id, CancellationToken ct);
+        Task<(List<Order> Items, int TotalCount)> GetAllAsync(int pageNumber, int pageSize, CancellationToken ct);
+        Task AddAsync(Order order, CancellationToken ct);
+        Task UpdateAsync(Order order, CancellationToken ct);
+        Task<bool> DeleteAsync(Guid id, CancellationToken ct);
     }
 }

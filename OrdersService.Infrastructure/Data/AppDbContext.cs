8251c614330031173df63ec7c24951fc148272a1
lastnurik <es.nurik3@gmail.com>
2025-10-28 14:07:41 +0500

    Отделил конфигурацию Order entity в отдельный класс

--- OrdersService.Infrastructure/Data/AppDbContext.cs
+++ OrdersService.Infrastructure/Data/AppDbContext.cs
@@ -14,14 +14,8 @@ namespace OrdersService.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Order>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.CreatedAt).IsRequired();
-                entity.Property(e => e.TotalAmount).IsRequired().HasColumnType("decimal(18,2)");
-                entity.Property(e => e.Status).IsRequired().HasConversion<string>();
-                entity.Property(e => e.Description).HasMaxLength(500);
-            });
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         }
     }
 }
---
+++ OrdersService.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrdersService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersService.Infrastructure.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.CreatedAt)
+                .IsRequired();
+
+            builder.Property(e => e.TotalAmount)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(e => e.Status)
+                .IsRequired()
+                .HasConversion<string>();
+
+            builder.Property(e => e.Description)
+                .HasMaxLength(500);
+        }
+    }
+}
